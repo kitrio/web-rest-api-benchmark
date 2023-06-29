522c2942d04ca870e9802fbf01f217e236a38d87
kit rio <mario64aq@gmail.com>
2023-06-30 01:12:04 +0900

    update laravel 10

--- php-laravel/app/Http/Controllers/Controller.php
+++ php-laravel/app/Http/Controllers/Controller.php
@@ -5,13 +5,12 @@ namespace App\Http\Controllers;
 use App\Model\Country;
 use App\Model\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests;
 
     public function hello()
     {
